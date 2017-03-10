1cc71ebeffc39be1645b33431410801ee360afd7
mininao <maxence_aici@yahoo.fr>
2017-03-10 09:39:20 +0100

    Adding products document example

---
+++ documents/products.js
@@ -0,0 +1,36 @@
+Products
+{
+    product_id: 1,
+    type: "menu",
+    price: 5.99,
+    quantity: 999,
+    name: "Menu Gourmand",
+    description: "Délicieux menu miam coin coin pouet",
+    img: "http://url_image",
+    options: [
+    {
+        option_name: ”Boisson Chaude”,
+        choices: [
+        {
+            name: "Chocolat Chaud",
+            img: "http://url_image"
+        },
+        {
+            name: "Café",
+            img: "http://url_image"
+        }]
+    },
+    {
+        option_name: ”Viennoiserie”,
+        choices: [
+        {
+            name: "Pain au chocolat",
+            img: "http://url_image"
+        },
+        {
+            name: "Croissant",
+            img: "http://url_image"
+        }]
+    }]
+
+}
