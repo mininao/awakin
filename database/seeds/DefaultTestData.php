2c200fddfa2289c20e8795acc637c49eb9d94da2
mininao <maxence_aici@yahoo.fr>
2017-03-24 16:38:11 +0100

    Added seeder for Products, Users, Orders, and OrderedProducts

--- database/seeds/DefaultTestData.php
+++ database/seeds/DefaultTestData.php
@@ -1,32 +1,15 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Log;
+
 use App\User;
 use App\Order;
 use App\Product;
 
 class DefaultTestData extends Seeder
+
 {
-
-  /**
-   * The Faker instance for the Seeder.
-   *
-   * @var \Faker\Generator
-   */
-  protected $faker;
-
-  /**
-   * Create a new seeder instance.
-   *
-   * @param  \Faker\Generator  $faker
-   * @return void
-   */
-  public function __construct(Faker $faker)
-  {
-      $this->faker = $faker;
-  }
-
     /**
      * Run the database seeds.
      *
@@ -34,40 +17,44 @@ class DefaultTestData extends Seeder
      */
     public function run()
     {
-      // Create one main test user
-      $faker = Faker\Factory::create();
 
-      User::create([
-          'name' => "Max",
-          'email' => "max@max.max",
-          'password' => bcrypt("maxmax"),
-      ]);
-
-      // And a bunch of others
-
-      factory(User::class, 10)->create();
-
-      // Create some products
-
-      $products = factory(Product::class, 10)->create();
-
-      // Create some orders for each User
-
-      $users = User::all();
-
-      $users->each(function ($u) {
-        for ($x = 0; $x <= $faker->numberBetween(5,10); $x++) {
-          $u->orders()->save(factory(App\Order::class)->make());
+        // Create a test user, if it doesn't already exists.
+        if(!User::first()) {
+            $this->command->info('Zero users found, generating test user');
+            User::create([
+                'name' => "Max",
+                'email' => "max@max.max",
+                'password' => bcrypt("maxmax")
+            ]);    
         }
-      });
+        
+        // And a bunch of others
+        $this->command->info('Generating ten more users');
+        factory(User::class , 10)->create();
 
-      // Populate the orders with ordered products
+        // Create some products
+        $this->command->info('Generating 10 Products');
+        $products = factory(Product::class , 10)->create();
 
-      $orders = User::all();
+        // Create some orders for each User
+        $users = User::all();
+        $users->each(function ($u)
+        {
+            $this->command->info('Generating Orders for one user');
+            for ($x = 0; $x <= rand(1, 3); $x++)	{
+                $u->orders()->save(factory(Order::class)->make());
+            }
+        });
 
-      $orders->each(function ($order) {
-        $order->products()->saveMany($faker->randomElements($products,$faker->numberBetween(1,4)));
-      });
-
+        // Populate the orders with ordered products
+        $orders = Order::all();
+        $this->command->info('Will now populate '.$orders->count().' Orders');
+        $orders->each(function ($order) use ($products)
+        {
+            $this->command->info('Populating order #' . $order->order_id . ' with products');
+            for ($x = 0; $x <= rand(1, 4); $x++)	{
+                $order->products()->save($products->random());
+            }
+        });
     }
 }
