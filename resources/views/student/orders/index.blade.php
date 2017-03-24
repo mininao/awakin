502c29f66f5a299ba75c43d2003a8ce861bef8f4
mininao <maxence_aici@yahoo.fr>
2017-03-24 17:18:55 +0100

    Proper Order list page

--- resources/views/student/orders/index.blade.php
+++ resources/views/student/orders/index.blade.php
@@ -5,14 +5,20 @@
     <div class="row">
         <div class="">
             <h1>Mes commandes</h1>
-            <ul>
-              @foreach ($orders as $order)
-                  <li>
-                      {{ $order->status }}
-                      {{ $order->products }}
-                  </li>
-              @endforeach
-            </ul>
+            <hr>
+            @foreach ($orders as $order)
+                <div>
+                    <h2>Commande #{{ $order->order_id }}</h2>
+                    <h3>Status : {{ $order->status }}</h3>
+                    <h3>Produits :</h3>
+                    <ul>
+                        @foreach ($order->products as $product)
+                            <li>{{ $product->title }} à {{ $product->price/100 }} euros</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <hr>
+            @endforeach
         </div>
     </div>
 </div>
