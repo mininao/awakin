2cec3dfe3bdd56cd8768c30027a46f6431e999a9
mininao <maxence_aici@yahoo.fr>
2017-03-24 04:04:18 +0100

    Created default Models and Migrations for OrderedProduct, Product and OrderMessages

---
+++ database/migrations/2017_03_24_030252_create_ordered_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderedProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ordered_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ordered_products');
+    }
+}
