e000b3f595b333ecfc0a77f9d7154a7078f5a224
mininao <maxence_aici@yahoo.fr>
2017-03-24 04:30:18 +0100

    Added Many to Many relathionship between Order and Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+  public function orders()
+  {
+    return $this->belongsToMany('App\Order', 'ordered_products');
+  }
 }
