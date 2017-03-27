beca3e5e1e73f83f7e2224baafef3bb1b3114af3
mininao <maxence_aici@yahoo.fr>
2017-03-27 14:16:53 +0200

    Redirect to student dashboard after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/student/orders';
+    protected $redirectTo = '/student/';
 
     /**
      * Create a new controller instance.
