9849aa1cb0ac46d373033403e8790ec320dd6643
Mumu des Bois <mula.adri@gmail.com>
2017-03-27 16:45:07 +0200

    Populate fields on Edit Form

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -3,7 +3,7 @@
     <label for="title">Nom du produit :</label>
     <input placeholder="Nom" name="title" type="text" value="">
     <label for="description">Description :</label>
-    <textarea placeholder="Description" name="description" cols="50" rows="10"></textarea>
+    <textarea placeholder="Description" name="description" cols="50" rows="10" value=""></textarea>
     <label for="price">Prix :</label>
     <input placeholder="Prix" name="price" type="text" value="">
     <input type="submit" value="Envoyer !">
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -2,10 +2,10 @@
     {{ csrf_field() }}
     {{ method_field('PUT') }}
     <label for="title">Nom du produit :</label>
-    <input placeholder="Nom" name="title" type="text" value="" id="title">
+    <input placeholder="Nom" name="title" type="text" value="{{ $product->title }}">
     <label for="description">Description :</label>
-    <textarea placeholder="Description" name="description" cols="50" rows="10" id="description"></textarea>
+    <textarea placeholder="Description" name="description" cols="50" rows="10">{{ $product->description }}</textarea>
     <label for="price">Prix :</label>
-    <input placeholder="Prix" name="price" type="text" value="" id="price">
+    <input placeholder="Prix" name="price" type="text" value="{{ $product->price }}">
     <input type="submit" value="Sauvegarder les changements">
 </form>
