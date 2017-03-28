9ddabef706be6633404dc3e7759fd0cb7cfdd376
Mumu des Bois <mula.adri@gmail.com>
2017-03-28 09:46:41 +0200

    Route fix + install Socialite lib

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/student/';
+    protected $redirectTo = '/student';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/student';
 
     /**
      * Create a new controller instance.
