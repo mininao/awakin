8441c19810ea0e4b056ed861c3f968264cd45f0e
Vincent Salahkar <vincent.salahkar@gmail.com>
2017-04-23 21:37:23 +0200

    Order Create page design
    - design ok
    - to fix : JS for tabs navigation

--- resources/views/student/orders/create.blade.php
+++ resources/views/student/orders/create.blade.php
@@ -1,22 +1,46 @@
-@extends('layouts.student')
-
-@section('student-content')
+@extends('layouts.student') @section('student-content')
 <div class="container">
-    <div class="row">
-        <div class="">
+    <div class="row align-items-center">
+        <div class="col-md-6 col-md-offset-3 orders">
             <h1>Créer une commande</h1>
+            <i class="ios-cafe"></i>
             <form action="/student/orders" method="post">
                 {{ csrf_field() }}
                 <h4>Produits choisis :</h4> <br>
-                ID produit:<input type="text" name="product_ids[]" value="1">
-                Quantité : <input type="number" name="product_qtys[]" value="1"> <br>
-                ID produit:<input type="text" name="product_ids[]" value="2">
-                Quantité : <input type="number" name="product_qtys[]" value="1"> <br>
-                ID produit:<input type="text" name="product_ids[]" value="3">
-                Quantité : <input type="number" name="product_qtys[]" value="1"> <br>
-                ID produit:<input type="text" name="product_ids[]" value="4">
-                Quantité : <input type="number" name="product_qtys[]" value="1"> <br>
-                <input type="submit" value="Passer commande">
+                <ul class="nav nav-pills justify-content-around" id="myTab">
+                    <li role="presentation" class="active nav-item"><a href="#boisson"><i class="ion-ios-cafe"></i></a></li>
+                    <li role="presentation"><a href="#sucrerie" class="nav-item"><i class="ion-ios-ice-cream"></i></a></li>
+                    <li role="presentation"><a href="#" class="nav-item"><i class="ion-ios-pizza"></i></a></li>
+                </ul>
+                <div class="tab-content order-tab">
+                    <div class="tab-pane active" id="boisson" role="tabpanel">
+                        <table class="table product-table">
+                            <thead class="order-tab__head justify-content-around">
+                                <tr>
+                                    <th>Produit</th>
+                                    <th class="text-center">Prix</th>
+                                    <th class="text-center">Quantité</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" name="product_ids[]">Cappuccino</th>
+                                    <td class="text-center">0.60</td>
+                                    <td class="text-center">
+                                        <button type="button" name="product_qtys[]" class="order-tab__quantity-button order-tab__quantity-button--default">+</button>
+                                        <button type="button" name="product_qtys[]" class="order-tab__quantity-button order-tab__quantity-button--active">+</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="tab-pane" id="sucrerie" role="tabpanel">pane 2</div>
+                    <div class="tab-pane" id="messages" role="tabpanel">pane 3</div>
+                    <div class="tab-pane" id="settings" role="tabpanel">pane 4</div>
+                </div>
+                <div class="row justify-content-center">
+                    <input type="submit" value="Valider (montant total)">
+                </div>
             </form>
         </div>
     </div>
