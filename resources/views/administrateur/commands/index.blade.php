eafd49feb6c4adfe1f42deffb2ffc0c7b5b9c970
mininao <maxence_aici@yahoo.fr>
2017-04-27 11:19:17 +0200

    Use common app template in admin

--- resources/views/administrateur/commands/index.blade.php
+++ resources/views/administrateur/commands/index.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Commands</title>
-        <link rel="stylesheet" href="../css/app.css">
-        <script>
-            window.Laravel = {!! json_encode([
-                'csrfToken' => csrf_token(),
-            ]) !!};
-        </script>
+@extends('layouts.app')
 
-    </head>
-    <body>
-
-        <div id="orderlist">
-            <orderlist></orderlist>
-        </div>
-
-        <script src="../js/app.js"></script>
-    </body>
-</html>
+@section('content')
+    <orderlist></orderlist>
+@endsection
