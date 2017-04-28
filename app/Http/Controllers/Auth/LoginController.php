752fc47b2122b0ca046d1a4d41ef3480a16984c0
mininao <maxence_aici@yahoo.fr>
2017-04-28 10:26:31 +0200

    Login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/student/';
 
     /**
      * Create a new controller instance.
