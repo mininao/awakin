802f8745ebda86b7f93629fdd04899002d08e24a
mininao <maxence_aici@yahoo.fr>
2017-04-28 03:51:12 +0200

    Design status

--- resources/assets/js/components/GetStatus.vue
+++ resources/assets/js/components/GetStatus.vue
@@ -1,19 +1,18 @@
 <template>
 
     <div class="row justify-content-around text-center order-status">
-        {{order.status}}
-        <div class="status-icon status-icon--received">
-            <i class="ion-ios-checkmark-circle-outline"></i> Reçue
+        <!-- {{order.status}} -->
+        <div class="status-icon">
+            <i class="ion-ios-checkmark-circle-outline"></i> <p>Reçue</p>
         </div>
-        <div class="status-icon status-icon--in-progress" v-bind:class="[{ 'status-icon--in-progress--inactive': order.status == 'preparing' }]">
-            <i class="ion-ios-checkmark-circle-outline"></i> En Préparation
+        <div class="status-icon" v-bind:class="[{ 'status-icon--inactive': order.status == 'received' }]">
+            <i class="ion-ios-checkmark-circle-outline"></i> <p>En préparation</p>
         </div>
-        <div class="status-icon status-icon--in-progress" v-bind:class="[{ 'status-icon--in-progress--inactive': order.status == 'ready' }]">
-            <i class="ion-ios-checkmark-circle-outline"></i> Prête
+        <div class="status-icon" v-bind:class="[{ 'status-icon--inactive': (order.status == 'received' || order.status == 'preparing') }]">
+            <i class="ion-ios-checkmark-circle-outline"></i> <p>Prête</p>
         </div>
-        <div class="status-icon status-icon--delivered" v-bind:class="[{ 'status-icon--delivered--inactive': order.status != 'finished' }]">
-            <!-- <i class="ion-ios-checkmark-circle-outline"></i>  -->
-            Récupérée
+        <div class="status-icon" v-bind:class="[{ 'status-icon--inactive': order.status != 'finished' }]">
+            <i class="ion-ios-checkmark-circle-outline"></i> <p>Récupérée</p>
         </div>
     </div>
 </template>
