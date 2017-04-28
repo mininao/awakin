d55f5e8352273ecb69bf03bb3eb03cb3511187b6
Mumu des Bois <mula.adri@gmail.com>
2017-04-28 02:56:47 +0200

    update statusOrder

--- resources/views/student/orders/show.blade.php
+++ resources/views/student/orders/show.blade.php
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <h1>Commande #{{ $order->order_id }}</h1>
-            <p>Statut : <get-status orderid="{{$order->order_id}}"></get-status></p>
+            <p>Statut : </p>
             <table class="table product-table">
                 <thead class="order-tab__head justify-content-around">
                     <tr>
@@ -25,18 +25,7 @@
             <div class="order-total text-center">
                 total = {{ $order->price()/100 }} euros
             </div>
-            <div class="row justify-content-around text-center order-status">
-                <div class="status-icon status-icon--received status-icon--received--inactive">
-                    <i class="ion-ios-checkmark-circle-outline"></i> Reçue
-                </div>
-                <div class="status-icon status-icon--in-progress">
-                    <i class="ion-ios-checkmark-circle-outline"></i> En Préparation
-                </div>
-                <div class="status-icon status-icon--delivered status-icon--delivered--inactive">
-                    <!-- <i class="ion-ios-checkmark-circle-outline"></i>  -->
-                    Livrée
-                </div>
-            </div>
+            <get-status orderid="{{$order->order_id}}"></get-status>
         </div>
     </div>
 </section>
