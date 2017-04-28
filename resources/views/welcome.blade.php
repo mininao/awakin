da818f46272c25496799bfbf4749bc523fd58286
mininao <maxence_aici@yahoo.fr>
2017-04-28 09:48:00 +0200

    caca landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
             </div>
         </div>
         <div class="welcome-hero__cta row text-center">
-            <a href="/login" class="">Commandez !</a>
+            <a href="/redirect" class="">Commandez !</a>
         </div>
         <!-- <h1>Le <em>Réveil</em> des étudiants.</h1> -->
     </div>
